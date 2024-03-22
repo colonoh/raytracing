a7bf547b05ff87f44374584130f6ad00b2a0945e
Steve Warner <warner@kairosaerospace.com>
2024-03-21 20:52:23 -0700

    Add some typing

--- raytracing/main.py
+++ raytracing/main.py
@@ -4,11 +4,14 @@ import numpy as np
 from tqdm import tqdm
 
 
-def main():
-    image_width = 256
-    image_height = 256
+MAX_COLOR_VALUE: int = 255
 
-    print(f"P3\n{image_width} {image_height}\n255")
+
+def main():
+    image_width: int = 256
+    image_height: int = 256
+
+    print(f"P3\n{image_width} {image_height}\n{MAX_COLOR_VALUE}")
 
     for j in tqdm(range(image_height)):
         for i in range(image_width):
@@ -16,13 +19,14 @@ def main():
             g = j / (image_height - 1)
             b = 0
 
-            ir = int(255.999 * r)
-            ig = int(255.999 * g)
-            ib = int(255.999 * b)
+            ir = int(MAX_COLOR_VALUE * r)
+            ig = int(MAX_COLOR_VALUE * g)
+            ib = int(MAX_COLOR_VALUE * b)
 
             print(f"{ir} {ig} {ib}")
 
     print("Done!", file=sys.stderr)
+
 
 if __name__ == "__main__":
     main()
